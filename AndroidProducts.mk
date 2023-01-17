4c07ce635fa103b839a0defedd4580ed56a96f02
Sergeant Kakashi <aaron210987@gmail.com>
2023-01-18 00:34:27 +0100

    a50: Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_a50.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a50-user \
+    lineage_a50-userdebug \
+    lineage_a50-eng
